4e5fceca93a918e4e233ac7b6946edabd789cf7e
Sébastien Larivière <sebastien@lariviere.me>
2014-07-24 19:36:52 -0400

    Check for the base directory

--- repo.sh
+++ repo.sh
@@ -2,6 +2,9 @@
 # Author: Sébastien Larivière <sebastien@lariviere.me>
 # Simple script to create a centos repository local clone
 
+# Varialbes
+baseDir=/mnt/repo
+
 # Check if a process is already running 
 if [ -f /mnt/repo/lock ]; then
     echo "Updates via rsync already running."
@@ -13,7 +16,6 @@ fi
 # Create repository for all the default repository
 createRepo(){
   version=$1
-  
   createrepo -v "/mnt/repo/${version}/os"
   createrepo -v "/mnt/repo/${version}/updates"
   createrepo -v "/mnt/repo/${version}/extras"
@@ -21,14 +23,27 @@ createRepo(){
   createrepo -v "/mnt/repo/${version}/contrib"
 }
 
-if [ -d /mnt/repo/6.5 ] ; then
-    rsync  -avSHP --delete --exclude "local*" --exclude "fasttrack" --exclude "isos" --exclude "repodata" --exclude "i386" centos.mirror.iweb.ca::centos/7/ /mnt/repo/7
-    createRepo 6.5
-    rsync  -avSHP --delete --exclude "local*" --exclude "xen4" --exclude "SCL" --exclude "cr" --exclude "fasttrack" --exclude "isos" --exclude "repodata" --exclude "i386" centos.mirror.iweb.ca::centos/6.5/ /mnt/repo/6.5
-    createRepo 7
-    chown -R apache:apache /mnt/repo
-    chcon -R --reference=/var/www/html/ /mnt/repo
-    /bin/rm -f /mnt/repo/lock
-else
-    echo "Target directory not present."
-fi
+# Create the directory if it's not available
+checkDirectory(){
+    completeDir=${baseDir}/${1}
+    if [ ! -d $completeDir ] ; then
+	echo "[+] Creating reposiroty directory"
+	mkdir $completeDir
+    fi
+}
+
+# Get the latests packages
+checkDirectory 6.5
+rsync  -avSHP --delete --exclude "local*" --exclude "xen4" --exclude "SCL" --exclude "cr" --exclude "fasttrack" --exclude "isos" --exclude "repodata" --exclude "i386" centos.mirror.iweb.ca::centos/6.5/ ${baseDir}/6.5
+createRepo 6.5
+
+checkDirectory 7
+rsync  -avSHP --delete --exclude "local*" --exclude "fasttrack" --exclude "isos" --exclude "repodata" --exclude "i386" centos.mirror.iweb.ca::centos/7/ ${baseDir}/7
+createRepo 7
+
+# Fix the files permission
+chown -R apache:apache /mnt/repo
+chcon -R --reference=/var/www/html/ /mnt/repo
+
+# Unlock the process
+rm -f /mnt/repo/lock
